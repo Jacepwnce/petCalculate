0e4f884f262ba80e4382206ca587e2bae7410fb1
Anton <jacepwncere@yandex.ru>
2022-12-02 20:26:19 +0700

    writing func for calc

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+
+import displayCalc from './calc.js'
+function sum (a, b) {
+    return a + b;
+}
+
+function minus (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+export default function allClear () {
+    displayCalc.textConten = 0;
+}
